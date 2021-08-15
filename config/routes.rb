604e35b751f36241862d833186675c6c675c4178
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-15 14:09:47 +0900

    fixed mask/:id route because it was wrong

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   end
   resources :guests, only: [:destroy]
   get "/masks", to: "masks#index", as: :masks
-  get "/mask", to: "masks#show/:id", as: :mask
+  get "/mask/:id", to: "masks#show", as: :mask
 end
