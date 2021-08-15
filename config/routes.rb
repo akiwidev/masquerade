1a8744872f25b6678ff5dc673d45ee2e4755567d
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-15 14:08:29 +0900

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   end
   resources :guests, only: [:destroy]
   get "/masks", to: "masks#index", as: :masks
-  get "/masks", to: "masks#mask/:id", as: :mask
+  get "/mask", to: "masks#show/:id", as: :mask
 end
